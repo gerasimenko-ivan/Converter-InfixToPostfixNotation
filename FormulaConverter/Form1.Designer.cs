bdc3f31e0f112f71eec2dca82b095fb9f46833c1
gerasimenko-ivan <gistmp@gmail.com>
2012-11-09 15:13:45 +0300

    Added labels to form TestInput

--- FormulaConverter/Form1.Designer.cs
+++ FormulaConverter/Form1.Designer.cs
@@ -31,21 +31,23 @@
             this.txtInputFormula = new System.Windows.Forms.TextBox();
             this.txtOutputFormula = new System.Windows.Forms.TextBox();
             this.btnConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtInputFormula
             // 
-            this.txtInputFormula.Location = new System.Drawing.Point(12, 12);
+            this.txtInputFormula.Location = new System.Drawing.Point(56, 12);
             this.txtInputFormula.Name = "txtInputFormula";
-            this.txtInputFormula.Size = new System.Drawing.Size(419, 20);
+            this.txtInputFormula.Size = new System.Drawing.Size(375, 20);
             this.txtInputFormula.TabIndex = 0;
             // 
             // txtOutputFormula
             // 
-            this.txtOutputFormula.Location = new System.Drawing.Point(12, 67);
+            this.txtOutputFormula.Location = new System.Drawing.Point(56, 67);
             this.txtOutputFormula.Name = "txtOutputFormula";
             this.txtOutputFormula.ReadOnly = true;
-            this.txtOutputFormula.Size = new System.Drawing.Size(419, 20);
+            this.txtOutputFormula.Size = new System.Drawing.Size(375, 20);
             this.txtOutputFormula.TabIndex = 2;
             // 
             // btnConvert
@@ -58,11 +60,31 @@
             this.btnConvert.UseVisualStyleBackColor = true;
             this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Infix";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Postfix";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(449, 103);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnConvert);
             this.Controls.Add(this.txtOutputFormula);
             this.Controls.Add(this.txtInputFormula);
@@ -78,6 +100,8 @@
         private System.Windows.Forms.TextBox txtInputFormula;
         private System.Windows.Forms.TextBox txtOutputFormula;
         private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
